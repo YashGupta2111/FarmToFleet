cfce44def87b39a284b06ee7de024973a7a33e15
Yash Gupta <122914867+YashGupta2111@users.noreply.github.com>
2024-07-31 19:54:16 +0530

    Update config.js

--- Server/db/config.js
+++ Server/db/config.js
@@ -1,2 +1,2 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/Codeutsava');
+mongoose.connect('mongodb+srv://yashguptag114:Yg@29102105@cluster0.ynp2ulm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
